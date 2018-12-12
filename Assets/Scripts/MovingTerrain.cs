be0221a679e83a6eb7b0f86db2f005e6e4af662d
escornt <thomas.escorne@epitech.eu>
2018-12-12 15:52:03 +0900

    fixed terrain rotation

--- Assets/Scripts/MovingTerrain.cs
+++ Assets/Scripts/MovingTerrain.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovingTerrain : MonoBehaviour {
 
-    public float speed = 40f;
+    public float speed =0.5f;
     public float smooth = 40f;
     Quaternion baseRotation;
 
@@ -18,8 +18,20 @@ public class MovingTerrain : MonoBehaviour {
 
         if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
         {
-            transform.Rotate(Input.GetAxis("Vertical") * Vector3.forward * speed * Time.deltaTime);
-            transform.Rotate(Input.GetAxis("Horizontal") * Vector3.right * speed * Time.deltaTime);
+
+
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(Input.GetAxis("Vertical") * 100, Vector3.forward), speed * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(Input.GetAxis("Horizontal") * 100, Vector3.right), speed * Time.deltaTime);
+
+           // transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(Input.GetAxis("Vertical"), Vector3.forward), speed * Time.deltaTime);
+           // transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(Input.GetAxis("Horizontal"), Vector3.right), speed * Time.deltaTime);
+         
+            
+            
+            
+            //transform.Rotate(Input.GetAxis("Vertical") * Vector3.forward * speed * Time.deltaTime);
+            //transform.Rotate(Input.GetAxis("Horizontal") * Vector3.right * speed * Time.deltaTime);
         }
         else
             transform.rotation = Quaternion.RotateTowards(transform.rotation, baseRotation, smooth * Time.deltaTime);
