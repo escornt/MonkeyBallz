70c96ceea6fce901127a9ade8e78db21bbe121af
Samuel <samu.osborne@gmail.com>
2018-12-18 23:13:10 +0900

    Changed level 2 and added end game screen

--- Assets/Scripts/GoalReached.cs
+++ Assets/Scripts/GoalReached.cs
@@ -1,13 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoalReached : MonoBehaviour
 {
+    public GameObject goalText;
+    public string nextLevel = "Level2";
+
+    private void Start()
+    {
+        goalText.SetActive(false);
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
-            Debug.Log("Goal");
+        {
+            if (other.gameObject.GetComponent<PlayerScript>().nbKey > 0)
+            {
+                goalText.SetActive(true);
+                StartCoroutine(LoadLevelAfterDelay(3.0f));
+            }
+        }
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(nextLevel);
     }
 }
